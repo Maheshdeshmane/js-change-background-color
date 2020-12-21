2fdf74454ebeffd92a71071420a10301e03b1eaf
Maheshdeshmane <mahesh.deshmane@yahoo.com>
2020-12-21 16:08:21 +0530

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,11 @@
+document.getElementById("btn").addEventListener("click", function() {
+
+    document.querySelector('body').style.backgroundColor = 
+
+                            'rgb('+ Math.floor(Math.random() * 256) + ',' 
+
+                                + Math.floor(Math.random() * 256) + ',' 
+
+                                + Math.floor(Math.random() * 256)+ ')'
+
+  });
